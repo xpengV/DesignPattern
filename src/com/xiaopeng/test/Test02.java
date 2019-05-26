36d63fef5f4cee8858e1f8ee60257bfd6911366a
xpengV <a_xiao_peng@163.com>
2019-05-26 18:31:49 +0800

    20190526日提交代码

---
+++ src/com/xiaopeng/test/Test02.java
@@ -0,0 +1,18 @@
+package com.xiaopeng.test;
+
+/**
+ * -------------------------------------------------
+ *
+ * @ClassName Test02
+ * @Auther xiaopeng
+ * @Date 2019/5/26 18:16
+ * @Version 1.0
+ * @Description TODO
+ * -------------------------------------------------
+ */
+public class Test02 {
+
+    public void say(){
+        System.out.println("123");
+    }
+}
